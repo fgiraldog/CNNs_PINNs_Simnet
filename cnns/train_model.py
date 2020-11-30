664d61d5c05b8752a11ed73a9048ff3219b87d6c
Felipe Giraldo Grueso <42279053+fgiraldog@users.noreply.github.com>
2020-11-30 11:53:21 -0500

    comments added

--- cnns/train_model.py
+++ cnns/train_model.py
@@ -2,26 +2,28 @@ import numpy as np
 import torch
 import copy
 
+# loss function 
 def loss_func(model, tensors,channels_weights):
 	
 	x_train, y_train = tensors
 	out = model(x_train)
 
-	lossu = ((out[:,0,:,:] - y_train[:,0,:,:]) ** 2).reshape((out.shape[0],1,out.shape[2],out.shape[3])) 
-	lossv = ((out[:,1,:,:] - y_train[:,1,:,:]) ** 2).reshape((out.shape[0],1,out.shape[2],out.shape[3])) 
-	lossp = torch.abs((out[:,2,:,:] - y_train[:,2,:,:])).reshape((out.shape[0],1,out.shape[2],out.shape[3])) 
-	loss = (lossu + lossv + lossp)/channels_weights
+	lossu = ((out[:,0,:,:] - y_train[:,0,:,:]) ** 2).reshape((out.shape[0],1,out.shape[2],out.shape[3])) # l2 regularization (u-field)
+	lossv = ((out[:,1,:,:] - y_train[:,1,:,:]) ** 2).reshape((out.shape[0],1,out.shape[2],out.shape[3])) # l2 regularization (v-field)
+	lossp = torch.abs((out[:,2,:,:] - y_train[:,2,:,:])).reshape((out.shape[0],1,out.shape[2],out.shape[3])) # l1 regularization (pressure-field)
+	loss = (lossu + lossv + lossp)/channels_weights # normalization
 	
 	return torch.sum(loss), out
 
+# training procedure per epoch
 def epoch_train(model, optimizer, batch, training, channels_weights):
 	
 	total_loss = 0
 
 	if training == True:
-		model.train()
+		model.train() # training
 	else:
-		model.eval()
+		model.eval() # validation
 
 	for element in batch:
 		optimizer.zero_grad()
@@ -34,6 +36,7 @@ def epoch_train(model, optimizer, batch, training, channels_weights):
 
 	return total_loss
 
+# training of the model
 def train(model, epochs, optimizer, train_dataset, val_dataset, channels_weights, batch_size):
 
 	loss_train = []
@@ -45,11 +48,11 @@ def train(model, epochs, optimizer, train_dataset, val_dataset, channels_weights
 	for epoch in range(0, epochs):
 		print('Epoch: {}/{}'.format(epoch+1,epochs))
 
-		#Training
+		# training
 		train_loss = epoch_train(model, optimizer, train_loader, True, channels_weights)
 		print('Train Loss = {}'.format(train_loss))
 
-		# Validation
+		# validation
 		with torch.no_grad():
 			val_loss = epoch_train(model, optimizer, val_loader, False, channels_weights)
 			print('Validation Loss = {}'.format(val_loss))
@@ -58,7 +61,7 @@ def train(model, epochs, optimizer, train_dataset, val_dataset, channels_weights
 		loss_train.append(train_loss)
 		loss_val.append(val_loss)
 
-		# Selection
+		# selection
 		if epoch == 0:
 			val_min = val_loss
 		if val_loss < val_min:
